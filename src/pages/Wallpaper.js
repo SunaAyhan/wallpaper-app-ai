92c2c44167af5da89c84898bd0247de1010a432c
Ömer AKGÜL <37873266+tuhalf@users.noreply.github.com>
2023-07-12 00:22:09 +0300

    Fixed Download and Replaced set as wallpaper button with Share

--- src/pages/Wallpaper.js
+++ src/pages/Wallpaper.js
@@ -5,41 +5,85 @@ import GetAppIcon from '@mui/icons-material/GetApp';
 import { useLocation } from "react-router";
 import { Filesystem, Directory, Encoding } from '@capacitor/filesystem';
 import { CapacitorHttp } from '@capacitor/core';
+import { Share } from '@capacitor/share';
 import {Plugins} from '@capacitor/core';
+import { Media, MediaSaveOptions } from "@capacitor-community/media";
+import { useState } from "react";
 const { Wallpaper } = Plugins;
 
 function WallpaperPage() {
     //get the generated wallpaper from the location state
     const generatedWallpaper = useLocation().state.generatedWallpaper;
-// helper function
-    const convertBlobToBase64 = (blob) => new Promise((resolve, reject) => {
-        const reader = new FileReader;
-        reader.onerror = reject;
-        reader.onload = () => {
-            console.log(blob);
-            resolve(reader.result);
-        };
-        reader.readAsDataURL(blob);
-        });
+    const [status, setStatus] = useState("");
+
+    const ensureDemoAlbum = async () => {
+        const { albums } = await Media.getAlbums();
+        const demoAlbum = albums.find(a => a.name === "Anime Wallpapers");
+        if (demoAlbum) {
+            setStatus("Demo album already exists!");
+            return demoAlbum.identifier;
+        }
+
+        await Media.createAlbum({ name: "Anime Wallpapers" });
+        setStatus("Created demo album");
+
+        return demoAlbum.identifier;
+    };
+    
     const handleDownloadClick = async () => {
+        const filePermission = await Filesystem.requestPermissions();
+        // fetch the image with the Capacitor Http plugin
+        // const response = await CapacitorHttp.request({
+        // method: 'GET',
+        // url: generatedWallpaper.output[0].image,
+        // responseType: 'blob'
+        // }).then(async (response) => {
+        //     const savedFile = await Filesystem.writeFile({
+        //     path: generatedWallpaper.id+".png",
+        //     data: response.data,
+        //     directory: Directory.External
+        //     });
+        //     //share the file
+        //     await Share.share({
+        //         title: 'Share',
+        //         text: 'Share this wallpaper',
+        //         url: savedFile.uri,
+        //         dialogTitle: 'Share this wallpaper'
+        //     });
+        //     alert("File saved to: " + savedFile.uri);
+        // }).catch((err) => {
+        //     alert("err: " + err);
+        // });
+        
+        let opts = { path: generatedWallpaper.output[0].image, albumIdentifier: await ensureDemoAlbum() };
+        await Media.savePhoto(opts);
+        alert("Saved to gallery");
+    }
+
+    //share the wallpaper
+    const handleShareClick = async () => {
         // fetch the image with the Capacitor Http plugin
         const response = await CapacitorHttp.request({
         method: 'GET',
         url: generatedWallpaper.output[0].image,
         responseType: 'blob'
         }).then(async (response) => {
-            const blob = new Blob( [ response.data ], { type: "image/png" } );
-            const base64Data = await convertBlobToBase64(blob);
             const savedFile = await Filesystem.writeFile({
             path: generatedWallpaper.id+".png",
-            data: base64Data,
-            directory: Directory.Data
+            data: response.data,
+            directory: Directory.External
+            });
+            //share the file
+            await Share.share({
+                title: 'Share',
+                text: 'Created with Sunayumi Anime Wallpaper Generator',
+                url: savedFile.uri,
+                dialogTitle: 'Share this wallpaper'
             });
             alert("File saved to: " + savedFile.uri);
         }).catch((err) => {
             alert("err: " + err);
         });
-        
     }
 
     //set the wallpaper as the device wallpaper
@@ -50,10 +94,8 @@ function WallpaperPage() {
             url: generatedWallpaper.output[0].image,
             responseType: 'blob'
             }).then(async (response) => {
-                const blob = new Blob( [ response.data ], { type: "image/png" } );
-                const base64Data = await convertBlobToBase64(blob);
                 Wallpaper.setBase64({
-                    base64: base64Data.replace(/^data:image\/(png|jpg);base64,/, ''),
+                    base64: response.data,
                     data: 'data:image/png;base64,'
                 }).catch(err => alert(err));
             }).catch((err) => {
@@ -109,8 +151,8 @@ function WallpaperPage() {
 
                 textTransform: "none",
             }}
-                onClick={handleSetAsWallpaperClick}  
-            variant="contained">Set As Wallpaper </Button>
+                onClick={handleShareClick}  
+            variant="contained">Share Wallpaper </Button>
 
         </div>
 
